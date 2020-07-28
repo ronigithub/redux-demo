8c7e90fd845b80c43da84522386fec1b24139ba5
Roni <ronicse2011@gmail.com>
2020-07-28 16:12:31 +0600

    combine reducer

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const redux = require("redux");
 const reduxLogger = require("redux-logger");
 
 const createStore = redux.createStore;
+// Middleware
 const applyMiddleware = redux.applyMiddleware;
-
+// Logger
 const logger = reduxLogger.createLogger();
+// Combine Reducer
+const combineReducer = redux.combineReducers;
 
+// Actions
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
@@ -14,11 +19,23 @@ function buyCake() {
   };
 }
 
-const initialState = {
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
+// Initial States
+const initialCakeState = {
   numberOfCake: 10,
 };
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numberOfIceCream: 10,
+};
+
+// Reducers
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -33,7 +50,28 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer, applyMiddleware(logger));
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numberOfIceCream: state.numberOfIceCream - 1,
+      };
+      break;
+
+    default:
+      return state;
+      break;
+  }
+};
+
+// Combine Reducer
+const rootReducer = redux.combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+//   Store
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 console.log("initial state", store.getState());
 
@@ -41,5 +79,7 @@ const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
 unsubscribe();
